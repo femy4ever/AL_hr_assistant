3ac787f16a2163ea4ea46303271cfe72be31c9a2
James Hardy <james.hardy@airelogic.com>
2025-11-20 11:28:01 +0000

    minimal working ui

---
+++ hr_assistant/Home.py
@@ -0,0 +1,34 @@
+import streamlit as st
+from interface import Interface
+from pathlib import Path
+from dotenv import load_dotenv
+from load_policies import load_policies
+from typing import List
+
+load_dotenv()
+
+st.header("HR helper PoC")
+
+@st.cache_resource
+def _load_policies(policy_path: str):
+    return load_policies(policy_path)
+
+@st.cache_resource
+def load_and_add_policies(policies: List[str]):
+    chatbot = Interface("hr_policies")
+    for name, text in policies.items():
+        chatbot.add_policy(name, text)
+    return chatbot
+
+policies = _load_policies("./Policies")
+chatbot = load_and_add_policies(policies)
+
+for name, text in policies.items():
+    with st.expander(f"Policy: {name}"):
+        st.write(text)
+
+prompt = st.chat_input("Ask")
+if prompt:
+    chatbot_response = chatbot.ask(prompt)
+    st.chat_message("user").write(prompt)
+    st.chat_message("assistant").write(chatbot_response)
