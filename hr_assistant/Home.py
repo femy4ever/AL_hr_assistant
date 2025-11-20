0612d5fcfd359b972a41e37cf1469ee219226ecd
James Hardy <james.hardy@airelogic.com>
2025-11-20 12:07:09 +0000

    added limited chat history

--- hr_assistant/Home.py
+++ hr_assistant/Home.py
@@ -8,7 +8,10 @@ from load_policies import load_policies
 
 load_dotenv()
 
-st.header("HR helper PoC")
+if "message_history" not in st.session_state:
+    st.session_state.message_history = []
+else:
+    st.session_state.message_history = st.session_state.message_history[-10:]
 
 @st.cache_resource
 def _load_policies(policy_path: str):
@@ -29,12 +32,19 @@ def fake_streaming_response(response: str):
 policies = _load_policies("./Policies")
 chatbot = load_and_add_policies(policies)
 
+st.header("HR helper PoC")
+
 for name, text in policies.items():
     with st.expander(f"Policy: {name}"):
         st.write(text)
 
+for m in st.session_state.message_history:
+    st.chat_message(m[0]).write(m[1])
+
 prompt = st.chat_input("Ask")
 if prompt:
     chatbot_response = chatbot.ask(prompt)
+    st.session_state.message_history.append(("user", prompt))
+    st.session_state.message_history.append(("assistant", chatbot_response))
     st.chat_message("user").write(prompt)
     st.chat_message("assistant").write_stream(fake_streaming_response(chatbot_response))
